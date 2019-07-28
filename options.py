cca309f6a56b16dd79bb0d937016abd88e4409c0
unknown <rockzhou15@gmail.com>
2019-07-28 14:23:38 -0400

    attempt 2

--- options.py
+++ options.py
@@ -4,6 +4,6 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--numImages', type=int, default=0, help='number of images to use in training')
 parser.add_argument('--batchSize', type=int, default=16, help='input batch size')
-parser.add_argument('--numEpochs', type=int, default=50, help='number of epochs to train for')
+parser.add_argument('--numEpochs', type=int, default=25, help='number of epochs to train for')
 
 opts = parser.parse_args()
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
 
     import numpy as np
-    from torch import optim, nn, cuda, LongTensor, FloatTensor, ones, zeros, from_numpy, save
+    from torch import optim, nn, cuda, LongTensor, FloatTensor, ones, zeros, from_numpy, save, rand
     import tqdm
     from dataset import GenerateIterator
     import matplotlib.pyplot as plt
@@ -34,36 +34,47 @@ if __name__ == "__main__":
         total_gen_loss = 0
         total_disc_loss = 0
 
+        disc_steps = 1
+
         for images in progress_bar:
-            valid_image_labels = ones(images.shape[0]).type(LongTensor)
-            fake_image_labels = zeros(images.shape[0]).type(LongTensor)
+            # one sided label smoothing
+            # valid_image_labels = ones(images.shape[0]).type(LongTensor)
+            valid_image_labels = (1.2-0.7)*rand(images.shape[0]) + 0.7
+            valid_image_labels = valid_image_labels.type(LongTensor)
+
+            # fake_image_labels = zeros(images.shape[0]).type(LongTensor)
+            fake_image_labels = (0.3)*rand(images.shape[0])
+            fake_image_labels = fake_image_labels.type(LongTensor)
 
             seed = from_numpy(np.random.normal(0,1, size=(images.shape[0], 100))).type(FloatTensor)
             if cuda.is_available():
                 valid_image_labels, fake_image_labels, images, seed = valid_image_labels.cuda(), fake_image_labels.cuda(), images.cuda(), seed.cuda()
 	        
 	        # --- train discriminator ---
-            optimizer_d.zero_grad()
+            # try stepping disc twice as much as gen
+            for i in range(disc_steps):
+                optimizer_d.zero_grad()
 
-            real_loss = adversarial_loss(disc(images), valid_image_labels)
-            real_loss.backward()
+                real_loss = adversarial_loss(disc(images), valid_image_labels)
+                real_loss.backward()
 
-            fake_images = gen(seed).detach()
-            fake_loss = adversarial_loss(disc(fake_images), fake_image_labels)
-            fake_loss.backward()
+                fake_images = gen(seed).detach()
+                fake_loss = adversarial_loss(disc(fake_images), fake_image_labels)
+                fake_loss.backward()
 
-	        # divide by 2 or else loss is weighted 2x towards disc vs gen
-            disc_loss = (real_loss + fake_loss) / 2
-            total_disc_loss += disc_loss.data
-	        
-            optimizer_d.step()
+    	        # divide by 2 or else loss is weighted 2x towards disc vs gen
+                disc_loss = (real_loss + fake_loss) / 2
+                total_disc_loss += disc_loss.data
+    	        
+                optimizer_d.step()
 
             # --- train generator ---
             disc = disc.eval()
 
             optimizer_g.zero_grad()
 
-            gen_loss = adversarial_loss(disc(fake_images), valid_image_labels)
+            # flip labels when training generator -> https://github.com/soumith/ganhacks
+            gen_loss = adversarial_loss(disc(fake_images), fake_image_labels)
             total_gen_loss += gen_loss.data
 	        
             gen_loss.backward()
@@ -71,12 +82,12 @@ if __name__ == "__main__":
 
             disc = disc.train()
 
-            progress_bar.set_description(f"disc loss: {disc_loss} || gen loss: {gen_loss}")
+            progress_bar.set_description(f"disc loss: {disc_loss.data} || gen loss: {gen_loss.data}")
 
         denom = len(image_iterator.dataset) // opts.batchSize
 
         avg_gen_loss = total_gen_loss / denom
-        avg_disc_loss = total_disc_loss / denom
+        avg_disc_loss = total_disc_loss / (disc_steps * denom)
         losses[0].append(avg_gen_loss)
         losses[1].append(avg_disc_loss)
 
