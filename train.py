18e00ff577a44c4bc40189c9b2149a0e243f3127
Tonyxu74 <Tonyxu74@hotmail.com>
2019-07-31 19:29:56 -0400

    Tony test (#1)
    * several tests to get non-noisy outputs
    
    Model made fully convolutional
    Settings should be optimized
    Input data has small annealed noise 
    Small adjustments to generate
    
    * added some sample outputs for DCGAN
    
    * change tabs to spaces
    
    * small adjustments

--- train.py
+++ train.py
@@ -1,79 +1,112 @@
+from torch import cuda
+import torch
+import tqdm
+from dataset import GenerateIterator
+import matplotlib.pyplot as plt
+from models import Generator, Discriminator
+from generate import generate_image
+from options import opts
+from models import weights_init
+
 if __name__ == "__main__":
 
-	import numpy as np
-	from torch import optim, nn, cuda, LongTensor, FloatTensor, ones, zeros, from_numpy, save
-	import tqdm
-	from dataset import GenerateIterator
-	import matplotlib.pyplot as plt
-	from models import Generator, Discriminator
-	from generate import generate_image
-	from options import opts
-	
-
 	# Loss function
-	adversarial_loss = nn.CrossEntropyLoss()
+	adversarial_loss = torch.nn.BCELoss()
 
+	# init models and apply custom weight initialization
 	gen = Generator()
+	gen.apply(weights_init)
+
 	disc = Discriminator()
+	disc.apply(weights_init)
 
-	optimizer_g = optim.Adam(gen.parameters(), lr=0.01, betas=(0.9,0.999))
-	optimizer_d = optim.Adam(disc.parameters(), lr=0.01, betas=(0.9,0.999))
+	# train from previously created model
+	if opts.continueTrain:
+		gen.load_state_dict(torch.load('./generator.pt'))
+		disc.load_state_dict(torch.load('./discriminator.pt'))
 
-	image_iterator = GenerateIterator('./images_cropped')
+	optimizer_g = torch.optim.Adam(gen.parameters(), lr=opts.lr, betas=(opts.beta1, opts.beta2))
+	optimizer_d = torch.optim.Adam(disc.parameters(), lr=opts.lr, betas=(opts.beta1, opts.beta2))
+
+	image_iterator = GenerateIterator('./images_cropped', shuffle=True)
+
+	# fix the noise so that we can see the image generated on the same noise every epoch
+	fixed_noise = torch.randn(opts.outputNum, opts.noiseSize, 1, 1)
 
 	if cuda.is_available():
-	    adversarial_loss = adversarial_loss.cuda()
-	    gen = gen.cuda()
-	    disc = disc.cuda()
-	    
+		adversarial_loss = adversarial_loss.cuda()
+		gen = gen.cuda()
+		disc = disc.cuda()
+
 	start_epoch = 1
-	losses = [[],[]]
+	losses = [[], []]
 
-	for epoch in range(start_epoch,opts.numEpochs):
-	    progress_bar = tqdm.tqdm(image_iterator)
-	    total_gen_loss = 0
-	    total_disc_loss = 0
+	for epoch in range(start_epoch, opts.numEpochs):
+		progress_bar = tqdm.tqdm(image_iterator)
+		total_gen_loss = 0
+		total_disc_loss = 0
 
-	    for images in progress_bar:
-	        valid_image_labels = ones(images.shape[0]).type(LongTensor)
-	        fake_image_labels = zeros(images.shape[0]).type(LongTensor)
+		# slowly anneal the standard deviation of the noise added to input, until it is removed fully at 30 epochs
+		standard_dev = 0.1 - (0.1/30) * epoch
+		if standard_dev < 0:
+			standard_dev = 0
+		image_iterator.dataset.std = standard_dev
 
-	        seed = from_numpy(np.random.normal(0,1, size=(images.shape[0], 100))).type(FloatTensor)
-	        if cuda.is_available():
-	            valid_image_labels, fake_image_labels, images, seed = valid_image_labels.cuda(), fake_image_labels.cuda(), images.cuda(), seed.cuda()
-	            
-	        # --- train generator ---
-	        fake_images = gen(seed)
-	        gen_loss = adversarial_loss(disc(fake_images), valid_image_labels)
-	        total_gen_loss += gen_loss.data
-	        
-	        optimizer_g.zero_grad()
-	        gen_loss.backward(retain_graph=True)
-	        optimizer_g.step()
-	        
-	        # --- train discriminator ---
-	        real_loss = adversarial_loss(disc(images), valid_image_labels)
-	        fake_loss = adversarial_loss(disc(fake_images), fake_image_labels)
-	        # divide by 2 or else loss is weighted 2x towards disc vs gen
-	        disc_loss = (real_loss + fake_loss) / 2
-	        total_disc_loss += disc_loss.data
-	        
-	        optimizer_d.zero_grad()
-	        disc_loss.backward(retain_graph=True)
-	        optimizer_d.step()
-	        
-	        progress_bar.set_description(f"disc loss: {disc_loss} || gen loss: {gen_loss}")
+		for batch_num, images in enumerate(progress_bar):
+			fake_image_labels = torch.ones(images.shape[0],)
+			valid_image_labels = torch.zeros(images.shape[0],)
 
-	    denom = opts.numImages // opts.batchSize
+			# try disc with soft labels, fake is between 0.9, 1.0, real between 0.0, 0.1
+			fake_image_labels_d = torch.rand(images.shape[0]) / 10 + 0.9
+			valid_image_labels_d = torch.rand(images.shape[0]) / 10
 
-	    avg_gen_loss = total_gen_loss / denom
-	    avg_disc_loss = total_disc_loss / denom
-	    losses[0].append(avg_gen_loss)
-	    losses[1].append(avg_disc_loss)
+			seed = torch.randn(images.shape[0], opts.noiseSize, 1, 1)
 
-	    save(gen.state_dict(), './generator.pt')
-	    generate_image('./generator.pt', epoch=epoch)
+			if cuda.is_available():
+				valid_image_labels = valid_image_labels.cuda()
+				fake_image_labels = fake_image_labels.cuda()
+				valid_image_labels_d = valid_image_labels_d.cuda()
+				fake_image_labels_d = fake_image_labels_d.cuda()
+				images = images.cuda()
+				seed = seed.cuda()
 
+			# ----- train generator -----
+			fake_images = gen(seed)
+			gen_loss = adversarial_loss(disc(fake_images), valid_image_labels)
+			total_gen_loss += gen_loss.data
+
+			optimizer_g.zero_grad()
+			gen_loss.backward()
+			optimizer_g.step()
+
+			# ----- train discriminator -----
+			pred_real = disc(images)
+			pred_fake = disc(fake_images.detach())
+			real_loss = adversarial_loss(pred_real, valid_image_labels_d)
+			fake_loss = adversarial_loss(pred_fake, fake_image_labels_d)
+
+			# divide by 2 or else loss is weighted 2x towards disc vs gen
+			disc_loss = (real_loss + fake_loss) / 2
+			total_disc_loss += disc_loss.data
+
+			optimizer_d.zero_grad()
+			disc_loss.backward()
+			optimizer_d.step()
+
+			progress_bar.set_description(
+				f"epoch: {epoch} || disc loss: {total_disc_loss/batch_num} || gen loss: {total_gen_loss/batch_num}"
+			)
+
+		denom = opts.numImages // opts.batchSize
+
+		avg_gen_loss = total_gen_loss / denom
+		avg_disc_loss = total_disc_loss / denom
+		losses[0].append(avg_gen_loss)
+		losses[1].append(avg_disc_loss)
+
+		torch.save(gen.state_dict(), './generator.pt')
+		torch.save(disc.state_dict(), './discriminator.pt')
+		generate_image(fixed_noise, epoch=epoch)
 
 	# display loss graph
 	plt.plot(losses[0], label="gen")
@@ -83,4 +116,3 @@ if __name__ == "__main__":
 	plt.legend(loc='upper left')
 	plt.show()
 
-	save(gen.state_dict(), './generator.pt')
